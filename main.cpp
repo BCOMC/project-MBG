06617505e6ecab48b47d8206c79864dd8d6bf216
RajunCajun1 <thecajunthatrages@gmail.com>
2017-01-17 14:38:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@
 using namespace std;
 int main()
 {
-	
+	/*
+	This is just a test, not working on it yet.
+	Once I recieve a storyboard for this game, I will make it.
+	How the game will be: something like lifeline.
+	Completely text-based.
+	*/
 }
