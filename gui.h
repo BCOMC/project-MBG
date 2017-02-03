93bf12fb2c8a6c9bb78c3f835c068b9e88e5f096
RajunCajun1 <thecajunthatrages@gmail.com>
2017-02-03 13:46:15 -0500

    Update gui.h

--- gui.h
+++ gui.h
@@ -1,4 +1,8 @@
-int guimain(string in1, string in2, string in3, string in4)
+int guimain(string in1, string in2, string in3, string in4, int loop)
 {
-
+	string[4] input = {in1, in2, in3, in4}
+	while(loop<4)
+	{
+		loop++;
+	}
 }
